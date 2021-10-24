62ebaf34a22d998c18d147565e8fa28000c9ccfe
Siyoon Jeon <jeoncue@gmail.com>
2021-10-24 18:57:05 +0900

    [Client] #9 modify:add animation at sidebar

--- src/components/common/sidebar/Sidebar.tsx
+++ src/components/common/sidebar/Sidebar.tsx
@@ -1,55 +1,47 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faArchive, faLayerGroup, faSignOutAlt, faTh, faUserCircle, faUsers } from '@fortawesome/free-solid-svg-icons';
 
-const Sidebar = (props: { setOpenSidebar: Function; }): JSX.Element => {
-
-	const SideBarBackgroundEl = useRef(null);
-
-	const handleCloseAuto = () => {
-		props.setOpenSidebar(false);
-	};
-
-	const SideBarBackgroundClick = (e: React.MouseEvent<Element, MouseEvent>): void => {
-		if (e.target === SideBarBackgroundEl.current) {
-			props.setOpenSidebar(false);
-		}
-	};
-
+const Sidebar = (): JSX.Element => {
 	return (
 		<>
-			<div
-				className='sidebar'
-				onClick={(e) => SideBarBackgroundClick(e)}
-				ref={SideBarBackgroundEl}
-			>
-				<div className='sidebar-container'>
-					<div className='sidebar-main'>
-						<ul className='sidebar-menutab-container'>
-							<li onClick={handleCloseAuto}>
-								<Link to='/mypage'><FontAwesomeIcon icon={faUserCircle} className='profile'/></Link>
-							</li>
-							<li onClick={handleCloseAuto}>
-								<Link to='/mypage'>마이페이지</Link>
-							</li>
-							<li onClick={handleCloseAuto}>
-								<Link to='/makebingo'>빙고 만들기</Link>
-							</li>
-							<li onClick={handleCloseAuto}>
-								<Link to='/watchbingo'>나의 빙고보기</Link>
-							</li>
-							<li onClick={handleCloseAuto}>
-								<Link to='/ranking'>Community</Link>
-							</li>
-							<li onClick={handleCloseAuto}>
-								<Link to=''>About us</Link>
-							</li>
-						</ul>
+			<nav className='nav'>
+				<div>
+					<Link to='/home' className='nav-logo'>
+						<FontAwesomeIcon icon={faLayerGroup} className='nav-icon' />
+						<span>Bucket Bingo</span>
+					</Link>
+
+					<div className='nav-list'>
+						<Link to='/mypage' className='nav-link active'>
+							<FontAwesomeIcon icon={faUserCircle} className='nav-icon' />
+							<span>마이페이지</span>
+						</Link>
+
+						<Link to='/makebingo' className='nav-link'>
+							<FontAwesomeIcon icon={faTh} className='nav-icon' />
+							<span>빙고만들기</span>
+						</Link>
+
+						<Link to='/watchbingo' className='nav-link'>
+							<FontAwesomeIcon icon={faArchive} className='nav-icon' />
+							<span>나의 빙고보기</span>
+						</Link>
+
+						<Link to='/ranking' className='nav-link'>
+							<FontAwesomeIcon icon={faUsers} className='nav-icon' />
+							<span>Community</span>
+						</Link>
 					</div>
 				</div>
-			</div>
+				<Link to='/home' className='nav-link'>
+					<FontAwesomeIcon icon={faSignOutAlt} className='nav-icon' />
+					<span>로그아웃</span>
+				</Link>
+			</nav>
+
 		</>
 	);
 };
